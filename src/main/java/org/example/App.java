9cab63c59a9cfcfa1fe0cf7f9d20cebacfa741be
Barbul Gergő <gergo.barbul@stud.u-szeged.u>
2020-04-17 01:07:29 +0200

    alert

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,13 +5,16 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * JavaFX App
@@ -25,14 +28,23 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
 
-        stage.setTitle("Hello JavaFX");
+        primaryStage.setOnCloseRequest(e -> {
+            Alert confirm = new Alert(Alert.AlertType.CONFIRMATION, null, ButtonType.YES, ButtonType.NO);
+            confirm.setTitle("Are you sure you want to exit?");
+            confirm.setHeaderText("Are you sure you want to exit?");
 
-        VBox root = new VBox();
-        Scene scene = new Scene(root, 500,300);
-        stage.setScene(scene);
+            Optional<ButtonType> answer = confirm.showAndWait();
 
-        stage.show();
+            if (answer.get() == ButtonType.NO) {
+                e.consume();
+            }
+        });
+
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
+        primaryStage.show();
+
     }
 }
