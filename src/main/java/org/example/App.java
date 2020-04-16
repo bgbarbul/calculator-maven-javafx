3940aeda7ff897611d55d7f3b32205cd0b430fdd
Barbul Gergő <gergo.barbul@stud.u-szeged.u>
2020-04-16 22:38:31 +0200

    First demo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,10 +20,29 @@ public class App extends Application {
 
     private static Scene scene;
 
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
 
         stage.setTitle("Hello JavaFX");
+
+        VBox root = new VBox();
+        Text msg = new Text("Kérlek add meg a neved!");
+        TextField mezo =new TextField();
+
+        Button exit = new Button("Exit");
+        exit.setOnAction( e -> { Platform.exit(); } );
+
+        mezo.setOnKeyReleased( e -> { msg.setText("Üdv " + mezo.getText()); });
+
+        root.getChildren().addAll(msg, mezo, exit);
+
+        Scene scene = new Scene(root, 500,300);
+        stage.setScene(scene);
+
         stage.show();
     }
 
@@ -37,8 +61,6 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 */
-    public static void main(String[] args) {
-        launch();
-    }
+
 
 }
