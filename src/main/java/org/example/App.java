81ff183825bed835c4edf77152e0d7e7f3b0042e
Barbul Gergő <gergo.barbul@stud.u-szeged.u>
2020-04-17 00:21:33 +0200

    t

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -40,12 +40,12 @@ public class App extends Application {
         root1.setCenter(new Label("Közép"));
 
         //HBox
-        HBox root = new HBox();
-        root.setSpacing(10);
-        root.setPadding(new Insets(10,10,10,10));
-        root.getChildren().addAll(new Button("Egy"), new Button("Kettő"), new Button("Három"));
+        HBox root2 = new HBox();
+        root2.setSpacing(10);
+        root2.setPadding(new Insets(10,10,10,10));
+        root2.getChildren().addAll(new Button("Egy"), new Button("Kettő"), new Button("Három"));
 
-        Scene scene = new Scene(root, 500,300);
+        Scene scene = new Scene(root2, 500,300);
         primarystage.setTitle("BorderPane ");
         primarystage.setScene(scene);
 
