178af805aac5bcd2b720f927d79572a695f1fd30
Barbul Gergő <gergo.barbul@stud.u-szeged.u>
2020-04-16 23:55:16 +0200

    clear

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -30,37 +30,9 @@ public class App extends Application {
         stage.setTitle("Hello JavaFX");
 
         VBox root = new VBox();
-        Text msg = new Text("Kérlek add meg a neved!");
-        TextField mezo =new TextField();
-
-        Button exit = new Button("Exit");
-        exit.setOnAction( e -> { Platform.exit(); } );
-
-        mezo.setOnKeyReleased( e -> { msg.setText("Üdv " + mezo.getText()); });
-
-        root.getChildren().addAll(msg, mezo, exit);
-
         Scene scene = new Scene(root, 500,300);
         stage.setScene(scene);
 
         stage.show();
     }
-
-    /*
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-*/
-
-
 }
