60d3bdcae3a19fe5932b331a32dc80e13dd2de60
Barbul Gergő <gergo.barbul@stud.u-szeged.u>
2020-04-17 00:06:28 +0200

    clear

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,42 +27,8 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primarystage) throws IOException {
 
-        stage.setTitle("Hello JavaFX");
-
-        VBox root = new VBox();
-        Text msg = new Text("Kérlek add meg a neved!");
-        TextField mezo =new TextField();
-
-        Button exit = new Button("Exit");
-        exit.setOnAction( e -> { Platform.exit(); } );
-
-        mezo.setOnKeyReleased( e -> { msg.setText("Üdv " + mezo.getText()); });
-
-        root.getChildren().addAll(msg, mezo, exit);
-
-        Scene scene = new Scene(root, 500,300);
-        stage.setScene(scene);
-
-        stage.show();
+        
     }
-
-    /*
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-*/
-
-
 }
